2ea6be2afe7dad570d8b378182463ca65249c086
Wai Myo Kyaw <shweboy@gmail.com>
2017-01-02 02:36:12 +0700

    routes hooked

--- server.js
+++ server.js
@@ -22,6 +22,40 @@ app.use(morgan('dev'));
 // Use the passport package in our application
 app.use(passport.initialize());
 
+app.use('/', routes);
+// app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: {}
+  });
+});
+
 // demo Route (GET http://localhost:8080)
 app.get('/', function(req, res) {
   res.send('Hello! The API is at http://localhost:' + port + '/api');
--- server/routes/route_index.js
+++ server/routes/route_index.js
@@ -9,7 +9,7 @@ router.get('/', (req, res, next) => {
     __dirname, '..', '..', 'client', 'views', 'index.html'));
 });
 
-router.get('/api/v1/todos', (req, res, next) => {
+router.get('/api/view_restaurants', (req, res, next) => {
   const results = [];
   // Get a Postgres client from the connection pool
   pg.connect(connectionString, (err, client, done) => {
@@ -20,7 +20,7 @@ router.get('/api/v1/todos', (req, res, next) => {
       return res.status(500).json({success: false, data: err});
     }
     // SQL Query > Select Data
-    const query = client.query('SELECT * FROM items ORDER BY id ASC;');
+    const query = client.query('select * from restaurant;');
     // Stream results back one row at a time
     query.on('row', (row) => {
       results.push(row);
@@ -33,7 +33,82 @@ router.get('/api/v1/todos', (req, res, next) => {
   });
 });
 
-router.post('/api/v1/todos', (req, res, next) => {
+router.get('/api/view_menu', (req, res, next) => {
+  const results = [];
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    // SQL Query > Select Data
+    const query = client.query('select * from restaurant a, menu b, restaurant_menu ab where a.id = ab.restaurant_id and ab.menu_id=b.id and ab.on_date=CURRENT_DATE;');
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results
+    query.on('end', () => {
+      done();
+      return res.json(results);
+    });
+  });
+});
+
+router.get('/api/view_votes', (req, res, next) => {
+  const results = [];
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    // SQL Query > Select Data
+    const query = client.query('select * from vote a, restaurant b where a.restaurant_id=b.id and a.on_date=CURRENT_DATE;');
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results
+    query.on('end', () => {
+      done();
+      return res.json(results);
+    });
+  });
+});
+
+router.get('/api/view_votes_per_day/:current_date', (req, res, next) => {
+  const results = [];
+  // Get a Postgres client from the connection pool
+  var current_date = req.params.current_date;
+  console.log(current_date);
+  pg.connect(connectionString, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    // SQL Query > Select Data
+    const query = client.query('select * from vote a, restaurant b where a.restaurant_id=b.id and a.on_date=CURRENT_DATE;');
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results
+    query.on('end', () => {
+      done();
+      return res.json(results);
+    });
+  });
+});
+
+
+router.post('/api/vote_restaurant', (req, res, next) => {
   const results = [];
   // Grab data from http request
   const data = {text: req.body.text, complete: false};
@@ -46,69 +121,10 @@ router.post('/api/v1/todos', (req, res, next) => {
       return res.status(500).json({success: false, data: err});
     }
     // SQL Query > Insert Data
-    client.query('INSERT INTO items(text, complete) values($1, $2)',
-    [data.text, data.complete]);
+    client.query('INSERT INTO vote(restaurant_id, on_date, user_id) values($1, $2, $3)',
+    [data.restaurant_id, data.on_date, data.user_id]);
     // SQL Query > Select Data
-    const query = client.query('SELECT * FROM items ORDER BY id ASC');
-    // Stream results back one row at a time
-    query.on('row', (row) => {
-      results.push(row);
-    });
-    // After all data is returned, close connection and return results
-    query.on('end', () => {
-      done();
-      return res.json(results);
-    });
-  });
-});
-
-router.put('/api/v1/todos/:todo_id', (req, res, next) => {
-  const results = [];
-  // Grab data from the URL parameters
-  const id = req.params.todo_id;
-  // Grab data from http request
-  const data = {text: req.body.text, complete: req.body.complete};
-  // Get a Postgres client from the connection pool
-  pg.connect(connectionString, (err, client, done) => {
-    // Handle connection errors
-    if(err) {
-      done();
-      console.log(err);
-      return res.status(500).json({success: false, data: err});
-    }
-    // SQL Query > Update Data
-    client.query('UPDATE items SET text=($1), complete=($2) WHERE id=($3)',
-    [data.text, data.complete, id]);
-    // SQL Query > Select Data
-    const query = client.query("SELECT * FROM items ORDER BY id ASC");
-    // Stream results back one row at a time
-    query.on('row', (row) => {
-      results.push(row);
-    });
-    // After all data is returned, close connection and return results
-    query.on('end', function() {
-      done();
-      return res.json(results);
-    });
-  });
-});
-
-router.delete('/api/v1/todos/:todo_id', (req, res, next) => {
-  const results = [];
-  // Grab data from the URL parameters
-  const id = req.params.todo_id;
-  // Get a Postgres client from the connection pool
-  pg.connect(connectionString, (err, client, done) => {
-    // Handle connection errors
-    if(err) {
-      done();
-      console.log(err);
-      return res.status(500).json({success: false, data: err});
-    }
-    // SQL Query > Delete Data
-    client.query('DELETE FROM items WHERE id=($1)', [id]);
-    // SQL Query > Select Data
-    var query = client.query('SELECT * FROM items ORDER BY id ASC');
+    const query = client.query('SELECT * FROM vote ORDER BY id ASC');
     // Stream results back one row at a time
     query.on('row', (row) => {
       results.push(row);
